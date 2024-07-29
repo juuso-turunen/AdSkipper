558a0e05d780f500a6b0b9c081915b2de05bd5a1
juuso.p.turunen@student.jyu.fi <44379448+juuso-turunen@users.noreply.github.com>
2024-07-30 00:33:38 +0300

    Refactoring and simplifying code

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,10 @@
 skipAd();
 
-function isAd() {
-    const player = document.querySelector(".html5-video-player.ad-showing");
-    if (player == null) return false;
-    return true;
-}
-
 function skipAd() {
-    if (!isAd()) return; // If there's no ad, then do nothing
+    const video = document.querySelector(".html5-video-player.ad-showing video"); // Basically this check if there's an ad
+    if (video) video.currentTime = video.duration;
 
-    const video = document.querySelector("video");
-    video.currentTime = video.duration;
-
+    // Sometimes it's required to press "Skip Ad" button. This handles it for user
     const skipAdButton = document.querySelector(".ytp-skip-ad-button");
-
-    if (skipAdButton != null) skipAdButton.click();
+    if (skipAdButton) skipAdButton.click();
 }
